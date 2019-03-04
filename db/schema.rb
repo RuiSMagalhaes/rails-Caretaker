a8ba7d20eb34ef5f3283ce682876b691e40f5b91
Emanuel Moniz <emanuelandremoniz@gmail.com>
2019-03-04 17:16:02 +0000

    add fields to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_143257) do
+ActiveRecord::Schema.define(version: 2019_03_04_171121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,20 @@ ActiveRecord::Schema.define(version: 2019_03_04_143257) do
     t.datetime "start_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
+    t.datetime "end_time"
+    t.boolean "recurring"
+    t.integer "minutes"
+    t.integer "hours"
+    t.integer "days"
+    t.integer "weeks"
+    t.integer "months"
+    t.integer "start_id"
+    t.boolean "notify_before"
+    t.boolean "notify_done"
+    t.boolean "notify_missed"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +48,5 @@ ActiveRecord::Schema.define(version: 2019_03_04_143257) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
