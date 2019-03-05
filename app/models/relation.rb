fca280d24d69cde84cc51ea5c09174ba27c9e153
Rui Magalhães <m.rui.s.magalhaes@gmail.com>
2019-03-05 10:59:54 +0000

    Associations on Relations table done

--- app/models/relation.rb
+++ app/models/relation.rb
@@ -1,2 +1,8 @@
 class Relation < ApplicationRecord
+  belongs_to :caretaker, foreign_key: 'caretaker_id', class_name: 'User'
+  belongs_to :patient, foreign_key: 'patient_id', class_name: 'User'
+
 end
+
+# permite fazer rel= Releation.create.....
+# rel.patient e rel.caretaker => User instance
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,16 @@ class User < ApplicationRecord
 
   mount_uploader :photo, PhotoUploader
 
-
   has_many :user_diseases
   has_many :diseases, through: :user_diseases
 
   has_many :notifications
 
   has_many :events
+
+  has_many :patient_relationships, foreign_key: :caretaker_id, class_name: 'Relation'
+  has_many :patients, through: :patient_relationships, source: :patient
+
+  has_many :caretaker_relationships, foreign_key: :patient_id, class_name: 'Relation'
+  has_many :caretakers, through: :caretaker_relationships, source: :caretaker
 end
