88ddc0407ef573e16043de11fd3f484f44ee79e1
Rui Magalhães <m.rui.s.magalhaes@gmail.com>
2019-03-06 11:30:27 +0000

    sidekiq dashboard available to users with admin privileges

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,12 @@ Rails.application.routes.draw do
   end
 
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  require "sidekiq/web"
+  authenticate :user, lambda { |u| u.admin } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
 end
 
 
