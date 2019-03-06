14c27d2d58aaeb773af4f1a40aa781c2af2aae42
Emanuel Moniz <emanuelandremoniz@gmail.com>
2019-03-06 15:42:38 +0000

    added js validation for recurring and for end date form to event

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,19 @@
 import "bootstrap";
+
+const eventsEdit = document.querySelector('.events.edit');
+const eventsNew = document.querySelector('.events.new');
+if (eventsEdit || eventsNew) {
+  const recurringTimes = document.querySelector('.event_recurring_times');
+  const eventHours = document.querySelector('.event_hours');
+  const eventDays = document.querySelector('.event_days');
+  const eventWeeks = document.querySelector('.event_weeks');
+  const eventMonths = document.querySelector('.event_months');
+  const checkBox = document.getElementById('event_recurring');
+  checkBox.addEventListener('change', (event) => {
+    recurringTimes.classList.toggle("hidden");
+    eventHours.classList.toggle("hidden");
+    eventDays.classList.toggle("hidden");
+    eventWeeks.classList.toggle("hidden");
+    eventMonths.classList.toggle("hidden");
+  })
+}
