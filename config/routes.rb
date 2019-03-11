c0fe28613d93cd1be73d1f4a7ea89081ad860b36
Emanuel Moniz <emanuelandremoniz@gmail.com>
2019-03-11 13:17:57 +0000

    nested notifications, changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :profiles, only: [:index, :show, :edit] do
+  resources :profiles, only: [:index, :show] do
     resources :events
+    resources :notifications, only: [:index, :show, :update, :destroy]
     get '/relations/new_caretaker', to: 'relations#new_caretaker', as: 'new_caretaker'
     post '/relations/new_caretaker', to: 'relations#create_caretaker', as: 'create_caretaker'
     get '/relations/new_patient', to: 'relations#new_patient', as: 'new_patient'
@@ -9,9 +10,8 @@ Rails.application.routes.draw do
   end
 
   # to have a index events page (with all events caretaker and all patients)
-  get '/events', to: 'events#schedule'
-
-  resources :notifications, only: [:index, :show, :update, :destroy]
+  get '/events', to: 'events#full_index', as: "events"
+  get '/notifications', to: 'notifications#full_index', as: "notifications"
 
   root to: 'pages#home'
 
