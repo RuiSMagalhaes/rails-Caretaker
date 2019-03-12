9ab3446fab8cfe3e5ea230c2b100a7e596e2027e
Rui Magalhães <m.rui.s.magalhaes@gmail.com>
2019-03-12 17:17:37 +0000

    meta data added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,10 @@ class ApplicationController < ActionController::Base
   #   redirect_to(root_path)
   # end
 
+  def default_url_options
+  { host: ENV["HOST"] || "localhost:3000" }
+  end
+
   protected
 
   def configure_permitted_parameters
---
+++ config/initializers/default_meta.rb
@@ -0,0 +1,15 @@
+DEFAULT_META = YAML.load_file(Rails.root.join("config/meta.yml"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
