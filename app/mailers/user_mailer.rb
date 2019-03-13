9fb1c8b0f1ef038da6f2227e7193fcdadc046ad6
Emanuel Moniz <emanuelandremoniz@gmail.com>
2019-03-13 00:01:39 +0000

    init implementations

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,21 @@
 class UserMailer < ApplicationMailer
+  include Devise::Mailers::Helpers
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.welcome.subject
-  #
+  def confirmation_instructions(record, token, opts = {})
+    @token = token
+    devise_mail(record, :confirmation_instructions, opts)
+  end
+
+  def reset_password_instructions(record, token, opts = {})
+    @token = token
+    devise_mail(record, :reset_password_instructions, opts)
+  end
+
+  def unlock_instructions(record, token, opts = {})
+    @token = token
+    devise_mail(record, :unlock_instructions, opts)
+  end
+
   def welcome(user)
     @user = user
 
