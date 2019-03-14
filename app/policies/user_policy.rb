b81f378068dc16a9e4f85fc677e9b2ec449a58a4
Emanuel Moniz <emanuelandremoniz@gmail.com>
2019-03-14 15:34:29 +0000

    fixed users relation apearing with pending request

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -6,11 +6,11 @@ class UserPolicy < ApplicationPolicy
   def show?
     # verify if profile is from a user that is patient of current user
     # chek if profile is from current user or if is from a patient
-    record == user || record.caretakers.include?(user)
+    record == user || record.caretakers.where('relations.state = true').include?(user)
   end
 
   def edit?
-    (record == user && !record.simple_view) || record.caretakers.include?(user)
+    (record == user && !record.simple_view) || record.caretakers.where('relations.state = true').include?(user)
   end
 
   def update?
